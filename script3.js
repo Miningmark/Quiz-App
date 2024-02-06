00619382b5d66821a2688f009c02bbea7703033c
Markus <markus.sibbe@t-online.de>
2024-02-06 11:04:07 +0100

    js change darkmode function

--- script3.js
+++ script3.js
@@ -7,35 +7,24 @@ function loadStyle(){
     switch (style) {
         case "dark":
             stylesheet.setAttribute('href', 'style-dark.css');
-            document.getElementById("test").checked = true;
-            //console.log("profile page");
-            //console.log(sessionStorage.getItem("syle"));
+            document.getElementById("checkbox-dark").checked = true;
             break;
         case "light":
             stylesheet.setAttribute('href', 'style-light.css');
-            //console.log("profile page");
-            //console.log(sessionStorage.getItem("syle"));
             break;
     }
 }
 
-
-window.onload = function() {
-    document.getElementById("test").addEventListener("click", toggleCheckbox());
-};
-
-
-function toggleCheckbox() {
-    if(document.getElementById("test").checked == true){
+document.getElementById("checkbox-dark").addEventListener("click", ()=>{
+    if(document.getElementById("checkbox-dark").checked == true){
         stylesheet.setAttribute('href', 'style-dark.css');
         sessionStorage.setItem("syle", "dark");
-        //console.log("profile page");
-        //console.log(sessionStorage.getItem("syle"));
     }else{
         stylesheet.setAttribute('href', 'style-light.css');
         sessionStorage.setItem("syle", "light");
     }
-}
+});
+
 
 
 
