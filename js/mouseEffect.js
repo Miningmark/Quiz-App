cf1e923b344372ec5eb4c09b7a16a300cfa7c407
Markus <markus.sibbe@t-online.de>
2024-02-16 00:00:43 +0100

    fixes for mouse effect

--- js/mouseEffect.js
+++ js/mouseEffect.js
@@ -4,12 +4,13 @@ const circle = document.getElementById("circle");
 
 let mousePosX = 0;
 let mousePosY = 0;
-let scrollX = 0;
-let scrollY = 0;
 let isX = 0;
 let isY = 0;
 let shouldX = 0;
 let shouldY = 0;
+const offSetY = 0;
+const borderTop = 100;
+const borderBottom = 52;
 
 document.addEventListener('mousemove', function (event) {
   mousePosX = event.clientX;
@@ -17,18 +18,20 @@ document.addEventListener('mousemove', function (event) {
   mouseEffect();
 });
 
-document.addEventListener("scroll", function (event) {
-  scrollX = window.scrollX;
-  scrollY = window.scrollY;
-  mouseEffect();
-});
 
 
 
 function mouseEffect(){
-  shouldX = mousePosX + scrollX;
-  shouldY = mousePosY + scrollY;
-  circle.style.top = shouldY + "px";
+  shouldX = mousePosX;
+  shouldY = mousePosY + offSetY;
+  console.log(shouldY);
+  if(shouldY < borderTop){
+    circle.style.top = borderTop + "px";
+  }else if(shouldY > borderTop && shouldY < window.innerHeight - borderBottom + offSetY){
+    circle.style.top = shouldY + "px";
+  }else{
+    circle.style.top = window.innerHeight - borderBottom + offSetY + "px";
+  }
   circle.style.left = shouldX + "px";
 }
 
